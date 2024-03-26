b6493261ecfc86839bdc796f5da5bb5cffc69d6a
AndreaRinaudo02 <162478987+AndreaRinaudo02@users.noreply.github.com>
2024-03-26 16:58:22 +0100

    Variazione formato output

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -10,28 +10,29 @@ int main()
     double N = 0;                            // N memorizza il numero di valori letti
 
     string file_input = "data.csv";
-    ifstream ifs(file_input);                // apre il file di input
+    ifstream ifs(file_input);              // apre il file di input
 
     string file_output = "result.csv";
-    ofstream ofs(file_output);               // apre il file di output
+    ofstream ofs(file_output);             // apre il file di output
 
     if (ifs.is_open())
     {
 
         if(ofs.is_open())
         {
+            ofs << "# N Mean" << endl;
             while(ifs >> x)
             {
-                if (ifs.fail())              // controlla possibili errori nell'estrazione di valori dal file di input
+                if (ifs.fail())            // controlla possibili errori nell'estrazione di valori dal file di input
                 {
                     cerr <<"Errore: impossibile estrarre un valore corretto da data.csv";
                 }
 
-                N = N+1;                     // aggiorna il numero di valori letti
-                x = (3./4.)*x - 7./4.;       // si usa una mappa lineare per mappare i valori nell'intervallo [-1;2]
+                N = N+1;                                   // aggiorna il numero di valori letti
+                x = (3./4.)*x - 7./4.;                     // si usa una mappa lineare per mappare i valori nell'intervallo [-1;2]
                 y = y + x;
                 double mean = y/N;
-                ofs << mean << "\n";         // stampa il valore della media in una nuova riga del file di output
+                ofs << N << " " << scientific << mean << "\n";      // stampa il valore della media in una nuova riga del file di output
             }
         }
 
@@ -46,7 +47,7 @@ int main()
         cerr << "Errore: impossibile aprire file di imput" << endl;
     }
 
-    ifs.close();                             // i file vengono chiusi (per sicurezza)
+    ifs.close();             // i file vengono chiusi (per sicurezza)
     ofs.close();
 
     return 0;
